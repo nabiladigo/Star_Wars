d0b96bb884b02711bdb7ab2ee782bf794f093b99
nabiladigo <nabiladigo91@gmail.com>
2021-12-15 22:04:49 -0500

    update act2.sh

--- act1.sh
+++ act1.sh
@@ -5,7 +5,7 @@ mkdir empire rebellion
 # 3. Inside the empire directory, create a file called darth_vader.txt 
 touch darth_vader.txt
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
-eco "...heavy breathing..."> darth_vader.txt
+echo "...heavy breathing..." > darth_vader.txt
 # 5. Inside the empire directory, create a file called emperor_palpatine.txt
 touch emperor_palpatine.txt
 # 6. Inside the empire directory, create a directory called death_star
